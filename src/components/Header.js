c8cbae3ab0306920f81af34556c1fff08a75f7f7
MANYA BHARDWAJ <114513010+bmanya24@users.noreply.github.com>
2023-11-03 11:37:09 +0530

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Manya Bhardwaj</h1>
+      <p>Software Engineer</p>
+    </header>
+  );
+}
+
+export default Header;
